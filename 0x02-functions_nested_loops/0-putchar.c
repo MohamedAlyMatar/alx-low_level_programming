b6bef88c811e862b45f7fc8ad8bdaab96f950779
“Mohamed <mohamedalymatar2001@gmail.com>
2023-03-21 14:13:48 +0200

    commit 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,13 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 int main(void)
 {
-	printf("_putchar");
+	char text[] = "_putchar";
+
+	for (int i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File name: main.h
+ *
+ * Description: Header file containing declartions for all function
+ */
+
+int _putchar(char c);
+int _putchar(char c){
+    putchar(c);
+}
+
+#endif
